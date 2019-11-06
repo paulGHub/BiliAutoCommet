54eaccb7fa206a4c55bd9852dc770807a9821f44
paulGHub <46396534+paulGHub@users.noreply.github.com>
2019-11-06 09:21:13 +0800

    test push

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Paul J
+ * @Description:
+ * @Date: created in 2019/11/6 9:16 , NCEPU(BaoDing)J10A202&203
+ * @Modified by:
+ */
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
